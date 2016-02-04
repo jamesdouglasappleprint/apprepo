1fa2745d1feac192a031d523aecf4d9da209d191
jamesdouglasappleprint <james.douglas@appleprint.co.uk>
2016-02-04 12:37:05 +0000

    moved pushwoosh to core

--- js/core.js
+++ js/core.js
@@ -578,7 +578,39 @@ document.addEventListener("deviceready", OnDeviceReady, false);
 function OnDeviceReady()    {
   console.log('device is ready')
   //Let's make a pet!
-  var Core = new Core();
-  initPushwoosh()
+  function initPushwoosh() {
+      console.log('pushwoosh init')
 
+      var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+
+      //set push notifications handler
+      document.addEventListener('push-notification', function(event) {
+          var title = event.notification.title;
+          var userData = event.notification.userdata;
+
+          if(typeof(userData) != "undefined") {
+              console.warn('user data: ' + JSON.stringify(userData));
+          }
+
+          alert(title);
+      });
+
+      //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+      pushNotification.onDeviceReady({ projectid: "", pw_appid : "4FF24-5ACEC" });
+
+      //register for pushes
+      pushNotification.registerDevice(
+          function(status) {
+              var pushToken = status;
+              console.warn('push token: ' + pushToken);
+          },
+          function(status) {
+              console.warn(JSON.stringify(['failed to register ', status]));
+          }
+      );
+  }
+
+  initPushwoosh()
 }
+
+var Core = new Core();
