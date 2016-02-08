628aba613d1c5345ab30d03237f8e6b24b8ea013
jamesdouglasappleprint <james.douglas@appleprint.co.uk>
2016-02-08 12:40:16 +0000

    Internal Test

--- js/core.js
+++ js/core.js
@@ -708,7 +708,7 @@ function OnDeviceReady()    {
       //set push notifications handler
       document.addEventListener('push-notification', function(event) {
         var notification = event.notification;
-        alert(notification.aps.alert);
+        //alert(notification.aps.alert);
         pushNotification.setApplicationIconBadgeNumber(0);
 
         alert('Push notification recieved!')
