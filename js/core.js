17e88e15ba0397d8e224ab9215811940983fdcd7
jamesdouglasappleprint <james.douglas@appleprint.co.uk>
2016-03-22 15:57:20 +0000

    Not enabled?

--- js/core.js
+++ js/core.js
@@ -1078,7 +1078,7 @@ Core.prototype.updateActionLevels = function(uid,firstLoad){
         localStorage.setItem("hasPet", true);
 
         //TODO: RENABLE
-        //core.initPushwoosh(localStorage.getItem("emailaddress"),data[0].pl,true)
+        core.initPushwoosh(localStorage.getItem("emailaddress"),data[0].pl,true)
         //console.log(firstLoad)
 
         if (prevPetLevel != data[0].pl && data[0].pl > 1 && firstLoad != 'firstload'){
